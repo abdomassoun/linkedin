4cd14ee3580941fc4f3773d637beee6f8a32dbd1
abdomassoun <abdemassoune@gmail.com>
2023-03-13 11:45:07 +0100

    add id

--- app/Http/Resources/certificateResource.php
+++ app/Http/Resources/certificateResource.php
@@ -15,7 +15,7 @@ class certificateResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-           
+           'id'=>$this->id,
             'name'=>$this->name,
             'age'=>$this->age,
             'note'=>$this->note,
